27be35d489efe43bd6639f148adda3a2fa3bffff
lxd <xdli@thoughtworks.com>
2014-06-17 10:52:47 +0800

    Add more helper

---
+++ helper/stop-bounce-on-client.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env sh
+
+# Distribute public key
+
+function do_dist() {
+    #ssh-copy-id twer@$1
+  PID=`ssh $1 ps aux | grep node | grep index.js | grep -v bash | awk '{print $2}'`
+  ssh -n $1 "kill $PID"
+}
+
+
+while read line; do
+    do_dist "$line"
+done < ../peers-list.txt
+
